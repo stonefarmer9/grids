ed51207cc5dcb8ad2a934f09cddf26f9c6f3d353
stonefarmer9 <g.p.levaillant@gmail.com>
2019-06-10 12:13:51 +0100

    CHALLENGE 2 complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './Challenge1.css';
+import './Challenge2.css'
 
 function App() {
   return (
-    <div className="Container">
-        <div className='One'> One </div>
-        <div className='Two'> Two </div>
-        <div className='Three'> Three </div>
+    <div className='App'>
+      <h2> Challenge 1</h2>
+      <div className="Container">
+          <div className='One'> One </div>
+          <div className='Two'> Two </div>
+          <div className='Three'> Three </div>
+      </div>
+      <br></br>
+      <br></br>
+      <h3>Challenge 2</h3>
+      <div className='Container2'>
+        <div className='uno'>One</div>
+        <div className='dos'>Two</div>
+        <div className='tres'>Three</div>
+        <div className='quattro'>Four</div>
+      </div>
     </div>
   );
 }
